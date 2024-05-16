dec575d258e0e3a0b7fe0d10e391c6e132dc8de7
Luken1919 <lukenfranco@gmail.com>
2024-05-16 08:50:08 +0200

    fondo aldatzen

---
+++ WB/Js/fondoAldatu.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const body = document.getElementById("indexbody");
+    const backgrounds = [
+        '../irudiak/fondo1.png',
+        '../irudiak/fondo2.png',
+        '../irudiak/fondo3.png',
+        '../irudiak/fondo4.png'
+    
+    ];
+    let currentBackgroundIndex = 0;
+
+    function changeBackground() {
+        currentBackgroundIndex = (currentBackgroundIndex + 1) % backgrounds.length;
+        body.style.backgroundImage = `url(${backgrounds[currentBackgroundIndex]})`;
+    }
+
+    setInterval(changeBackground, 4000); // Cambia cada 4 segundos
+});
